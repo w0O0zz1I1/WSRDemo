226cf281e2474d9ec8e4f91b5c8fe8bf385e4cf0
w0zz1 <gitwozzi@mail.ru>
2022-03-24 18:38:53 +0300

    Добавьте файлы проекта.

---
+++ WSRDemo/models/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSRDemo.models
+{
+    public class Client
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Surname { get; set; }
+        public string Phone { get; set; }
+    }
+}
